db78368c68072680ee410fb3813c5b99e6b61bc4
MichelBerg <michelberg99@gmail.com>
2022-08-18 09:43:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Base Project for Raspberry Pi 4 Development
+
+Currently depends on pigpio for GPIO access
+
+# Explaning the directory sructure
+
+src: contains all .cpp files
+include: contains all .h/.hpp files
+build: contains make files and the executable
+
+Todo:
+
+- Implement lib: directory in CMakeLists (recursive) this should be used for "own libraries"
---
+++ main.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file main.cpp
+ * @author your name (user@domain.com)
+ * @brief main with doxygen :)
+ * @version 0.1
+ * @date 2022-08-18
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <iostream>
+#include <pigpio.h>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    /**
+     * @brief This is just a small guide on how2work with this base Project.
+     */
+
+    // First Task: Try to execute "main"
+    cout << "Hello World!" << endl;
+    // Second Task:
+    // Create a simple Blinking sketch using pigpio
+
+    // Third Task
+    // Create and implement a class stored in src/include
+
+    // Fourth Task
+    // Commit your code
+
+    return EXIT_SUCCESS;
+}
